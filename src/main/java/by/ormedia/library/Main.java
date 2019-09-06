2d3a5bc2f8bd77f78eea0a5bab38ebe71aab78ff
misterlucky1982 <damonhill1996@tut.by>
2019-09-07 00:15:34 +0300

    initial commit

---
+++ src/main/java/by/ormedia/library/Main.java
@@ -0,0 +1,10 @@
+package by.ormedia.library;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/test/java/by/ormedia/library/AppTest.java
@@ -0,0 +1,14 @@
+package by.ormedia.library;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+	public void applicationTest() {
+		assertTrue(true);
+	}
+
+}
